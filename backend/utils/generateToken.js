1baaab5d958bb222da0c14c6fac129f3358223d2
unknown <vanshikaguptaaa2005@gmail.com>
2026-07-21 22:13:55 +0530

    Generate Token

---
+++ backend/utils/generateToken.js
@@ -0,0 +1,12 @@
+import jwt from 'jsonwebtoken'
+export const generateToken = (id, role) => {
+    return jwt.sign(
+        {
+            id, role
+        },
+        process.env.JWT_SECRET,
+        {
+            expiresIn: "1d"
+        }
+    )
+}
