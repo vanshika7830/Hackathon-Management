b1e2686fd28d04dffe554e14e26609256a946778
unknown <vanshikaguptaaa2005@gmail.com>
2026-07-22 18:42:44 +0530

    assign-judge route added

---
+++ backend/routes/hackathonRoutes.js
@@ -0,0 +1,22 @@
+import express from "express";
+import {
+    createHackathon,
+    getAllHackathons,
+    getHackathonById,
+    updateHackathon,
+    deleteHackathon,
+} from "../controllers/hackathonController.js";
+import { protect } from "../middleware/authMiddleware.js";
+import { authorizeRoles } from "../middleware/roleMiddleware.js";
+
+const router = express.Router();
+
+router.get("/", getAllHackathons);
+router.get("/:id", getHackathonById);
+
+router.post("/", protect, authorizeRoles("organizer"), createHackathon);
+router.put("/:id", protect, authorizeRoles("organizer"), updateHackathon);
+router.delete("/:id", protect, authorizeRoles("organizer", "admin"), deleteHackathon);
+router.patch("/:id/assign-judge", protect, authorizeRoles("organizer"), assignJudge);
+
+export default router;
